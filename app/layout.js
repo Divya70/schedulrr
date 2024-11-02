79e2ab5da52c2cae5b6c8f1ae80bb362371e309e
Divya <divyanamdev8795@gmail.com>
2024-11-02 14:38:30 +0530

    feat:connect with neonDB database

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 export const metadata = {
   title: "Schedulrr",
   description: "Meeting Scheduling App",
@@ -7,8 +9,25 @@ export const metadata = {
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* Header */}
+          <Header />
+
+          {/* Main Components */}
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
+
+          {/* Footer */}
+          <footer className="bg-blue-100 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with ❤ by Divya</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
